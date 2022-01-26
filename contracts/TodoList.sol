e569db8cea54ae05b84a7360d8f409b246187f08
manvith <iib2019014@iiita.ac.in>
2022-01-26 18:38:46 +0530

    1) changed TodoList - added Task, mapping, createtask 2) truffle compile, truffle migrate --reset

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,4 +3,23 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
     uint public taskCount;
+
+    struct Task {
+        uint id;
+        string description;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("Submit the lab session and lab assignment.");
+    }
+
+    function createTask(string memory content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, content, false);
+    }
+
+
 }
